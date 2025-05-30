938cad99c3eedac94c8c15d274056002d194fe66
Jesulayomy <jesulayomi081@gmail.com>
2025-05-30 15:53:09 -0400

    fix(posts-schema): Removed likes and required on image, title and description

--- models/Post.js
+++ models/Post.js
@@ -7,19 +7,12 @@ const PostSchema = new mongoose.Schema({
   },
   image: {
     type: String,
-    require: true,
   },
   cloudinaryId: {
     type: String,
-    require: true,
   },
   description: {
     type: String,
-    required: true,
-  },
-  likes: {
-    type: Number,
-    required: true,
   },
   postedBy: {
     type: mongoose.Schema.Types.ObjectId,
