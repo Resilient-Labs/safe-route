7f7fd3fe68031d738cf651680f65bd1af860b5a1
Jesulayomy <jesulayomi081@gmail.com>
2025-06-01 15:59:41 -0400

    check(comments): Verified comment functionality

--- controllers/comments.js
+++ controllers/comments.js
@@ -10,25 +10,21 @@ module.exports = {
         user: req.user.id,
         post: req.params.id
       });
-      console.log("Comment has been added!");
-      res.redirect("/post/" + req.params.id);
+      res.redirect('back');
     } catch (err) {
-      res.status(500).json({message:"Failed to create comment", error: err.message})
+      res.status(500).json({message:"Failed to create comment", error: err.message});
     }
   },
-  likeComment: async (req, res) => {
-    //TODO: Make sure to avoid duplicated likes and impplement toggle functionality    
-    
+  likeComment: async (req, res) => { 
     try {
-      const commentID = req.params.id
-      const userID = req.user.id
+      const commentID = req.params.id;
+      const userID = req.user.id;
 
       const comment = await Comment.findById(commentID);
-      if(!comment){
-        res.status(404).send("Comment Not Found")
+      if (!comment) {
+        res.status(404).send("Comment Not Found");
       }
-      // Check to make sure that the comment has been liked by the current user
-      const userHasLike = comment.likeBy.includes(userID)
+      const userHasLike = comment.likeBy.includes(userID);
 
       let operation;
       if(userHasLike){
@@ -36,39 +32,32 @@ module.exports = {
           $inc: {likes: -1},
           $pull: {likeBy: userID}
         }
-
-        console.log("Likes -1 (User unlinked)")
       }
       else{
         operation = {
           $inc: {likes: 1},
           $push: {likeBy: userID}
         }
-
-        Console.log("Likes +1 (User linked)")
       }
 
-      await Comment.findByIdAndUpdate(commentID, operation)
-      // To redirec back
-      res.redirect('back')
-
+      await Comment.findByIdAndUpdate(commentID, operation);
+      res.redirect('back');
     } catch (err) {
       console.log(err);
+      res.redirect('back');
     }
   },
   deleteComment: async (req, res) => {
     try {
-      //TODO: Make sure it works 
       const deleteComment = await Comment.findByIdAndDelete(req.params.id );
 
-      if(!deleteComment){
-        console.log("Comment not found for deletion:", req.params.id)
-        return res.redirect('back')
+      if (!deleteComment) {
+        console.log("Comment not found for deletion:", req.params.id);
+        return res.redirect('back');
       }
 
       console.log("Deleted Comment");
       res.redirect("back");
-
     } catch (err) {
       res.status(500).json({message: "Delelation was not succesful", error: err.message})
       res.redirect("back");
